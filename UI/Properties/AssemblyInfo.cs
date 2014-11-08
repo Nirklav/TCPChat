acae056bf8b80e4abba01c4713ee5980b1147636
Nirklav <mrAnton911@gmail.com>
2014-11-09 00:08:42 +0200

    Changed version;

--- UI/Properties/AssemblyInfo.cs
+++ UI/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.3.0.*")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
