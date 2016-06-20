2e665df5f632580102117e5ef058599795baab0d
Nirklav <mrAnton911@gmail.com>
2016-06-20 20:23:11 +0300

    Version changed;

--- Engine/Properties/AssemblyInfo.cs
+++ Engine/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Security;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
 
 [assembly: AllowPartiallyTrustedCallers]
