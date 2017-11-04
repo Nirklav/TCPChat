a4551c6c1d5718a1acc8eb461e187fd73d57ac79
Nirklav <mrAnton911@gmail.com>
2017-11-05 00:14:40 +0200

    Version update;

--- Engine/Properties/AssemblyInfo.cs
+++ Engine/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Security;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
 
 [assembly: AllowPartiallyTrustedCallers]
